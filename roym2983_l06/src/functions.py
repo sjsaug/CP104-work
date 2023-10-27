8f56d435de92881e09fabda1454f099696176927
August R.M <sjsaug@gmail.com>
2023-10-27 15:26:05 -0400

    l06 almost done

---
+++ roym2983_l06/src/functions.py
@@ -0,0 +1,94 @@
+def sum_even(num):
+    """
+    -------------------------------------------------------
+    Sums and returns the total of all even numbers from 2 to num (inclusive).
+    Use: total = sum_even(num)
+    -------------------------------------------------------
+    Parameters:
+        num - an integer (int > 0)
+    Returns:
+        total - sum of all even numbers from 2 to num (int)
+    ------------------------------------------------------
+    """
+    total = 0
+    for i in range(2, num + 1, 2):
+        total += i
+    return total
+
+
+def sum_odd(num):
+    """
+    -------------------------------------------------------
+    Sums and returns the total of all odd numbers from 1 to num (inclusive).
+    Use: total = sum_odd(num)
+    -------------------------------------------------------
+    Parameters:
+        num - an integer (int > 0)
+    Returns:
+        total - sum of all odd numbers from 1 to num (int)
+    ------------------------------------------------------
+    """
+    total = 0
+    for i in range(1, num + 1, 2):
+        total += i
+    return total
+
+def draw_triangle(height, char):
+    """
+    -------------------------------------------------------
+    Prints a triangle of height characters using
+    the char character.
+    Use: draw_triangle(height, char)
+    -------------------------------------------------------
+    Parameters:
+        height - number of characters high (int > 0)
+        char - the character to draw with (str, len() == 1)
+    Returns:
+        None
+    ------------------------------------------------------
+    """
+    for i in range(1, height + 1):
+        line = char * (2 * i - 1)
+        print(line.rjust(height + i - 1))
+    return
+
+def bottles_of_beer(n):
+    """
+    -------------------------------------------------------
+    Prints n verses of the song "99 Bottles of Beer on the Wall".
+    Use: bottles_of_beer(n)
+    -------------------------------------------------------
+    Parameters:
+        n - number of verses of the song to print (int > 0)
+    Returns:
+        None
+    ------------------------------------------------------
+    """
+    for x in range(n, 0, -1):
+        print(f"{x} bottles of beer on the wall, {x} bottles of beer.")
+        print(f"Take one down, pass it around, {x} bottles of beer on the wall.")
+    return
+
+def retirement(age, salary, increase):
+    """
+    -------------------------------------------------------
+    Calculates a prints a table of how much a worker earns
+    between age and retirement at 65.
+    Use: retirement(age, salary, increase)
+    -------------------------------------------------------
+    Parameters:
+        age - worker's current age (int > 0)
+        salary - worker's current salary (float > 0)
+        increase - percent increase in salary per year (float >= 0)
+    Returns:
+        None
+    ------------------------------------------------------
+    """
+    print(f"Age         Salary\n")
+    print(f"-------------------\n")
+    print(f"{age}         {salary}\n")
+    for i in range(age + 1, 66):
+        new_salary = salary * (i * increase)
+        print(f"{i}         {new_salary}\n")
+
+retirement(45, 40000, 2.0)
