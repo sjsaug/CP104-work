580652113a5c19df1ac44eea1fac961750910113
August R.M <sjsaug@gmail.com>
2023-10-29 19:09:50 -0400

    finish l08

--- roym2983_l08/src/functions.py
+++ roym2983_l08/src/functions.py
@@ -32,5 +32,87 @@ def get_lotto_numbers(n, low, high):
         numbers - a list of unique random lottery numbers (list of int)
     -------------------------------------------------------
     """
+    numbers = []
+    while len(numbers) < n:
+        number = randint(low, high)
+        if number not in numbers:
+            numbers.append(number)
+    numbers.sort()
+    return numbers
 
 
+def list_categorize(values):
+    """
+    -------------------------------------------------------
+    Returns data about the categories of values in a list.
+    Use: negatives, positives, zeroes, evens, odds = list_categorize(values)
+    -------------------------------------------------------
+    Parameters:
+        values - a list of values (list of int)
+    Returns:
+        negatives - the number of negative values (int)
+        positives - the number of positive values (int)
+        zeroes - the number of zeroes (int)
+        evens - the number of even values (int)
+        odds - the number of odd values (int)
+    -------------------------------------------------------
+    """
+    negatives = 0
+    positives = 0
+    zeroes = 0
+    evens = 0
+    odds = 0
+    for i in range(len(values)):
+        if values[i] < 0:
+            negatives += 1
+        elif values[i] > 0:
+            positives += 1
+        else:
+            zeroes += 1
+        if values[i] % 2 == 0:
+            evens += 1
+        else:
+            odds += 1
+    return negatives, positives, zeroes, evens, odds
+
+
+def many_search(values, value):
+    """
+    -------------------------------------------------------
+    Searches through values for value and returns a list of
+    all indexes of its occurrence.
+    User: indexes = many_search(values, value)
+    -------------------------------------------------------
+    Parameters:
+        values - a list of values (list of *).
+        value - can be compared to items in values (*).
+    Returns:
+        indexes - a list of indexes of the location of value in values,
+            [] if not found (list of int).
+    -------------------------------------------------------
+    """
+    indexes = []
+    for i in range(len(values)):
+        if values[i] == value:
+            indexes.append(i)
+    return indexes
+
+
+def list_sums(source1, source2):
+    """
+    -------------------------------------------------------
+    Calculates sums of elements of two lists. Lists must be the
+    same length.
+    Use: target = list_sums(source1, source2)
+    -------------------------------------------------------
+    Parameters:
+        source1 - list of numbers (list of float)
+        source2 - list of numbers (list of float)
+    Returns:
+        target - sums of elements of source1 and source2 (list of float)
+    -------------------------------------------------------
+    """
+    target = []
+    for i in range(len(source1)):
+        target.append(source1[i] + source2[i])
+    return target
