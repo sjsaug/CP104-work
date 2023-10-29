fd65215f20a68e7881f435a35dee78c58ace21d7
August R.M <sjsaug@gmail.com>
2023-10-29 19:10:37 -0400

    add formatting to l08

--- roym2983_l08/src/functions.py
+++ roym2983_l08/src/functions.py
@@ -1,4 +1,16 @@
+"""
+-------------------------------------------------------
+[program description]
+-------------------------------------------------------
+Author:  August Roy McLaughlin
+ID:      169052983
+Email:   roym2983@mylaurier.ca
+__updated__ = "2023-10-27"
+-------------------------------------------------------
+"""
+# Imports
 from random import randint
+# Constants
 
 def get_month_name(m):
     """
